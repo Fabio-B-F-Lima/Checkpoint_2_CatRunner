eb1a17defce478701de15e6eb89b49a9c6bb7636
Fabio-B-F-Lima <fabiobonifrolima@gmail.com>
2026-05-02 15:59:31 -0300

    PauseMenu and GameScene UI

--- CatRunner/Assets/_Project/Scripts/UIManager.cs
+++ CatRunner/Assets/_Project/Scripts/UIManager.cs
@@ -1,4 +1,7 @@
+using System;
+using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
@@ -10,13 +13,23 @@ public class UIManager : MonoBehaviour
     [Header("Canvas HUD")]
     [SerializeField] private Text fishCoinNumberText;
     [SerializeField] private Canvas HUD;
+    [SerializeField] private Button pauseButton, closePauseScreenButton, restartButton, quitButton;
+    [SerializeField] private GameObject pausePanel;
     
     void Start()
     {
         canvasDelay.enabled = true;
         HUD.enabled = false;
+        pausePanel.SetActive(false);
+
+
+        pauseButton.onClick.AddListener(PauseGame);
+        closePauseScreenButton.onClick.AddListener(ClosePauseScreen);
+        restartButton.onClick.AddListener(RestartGame);
+        quitButton.onClick.AddListener(BackToMenu);
     }
 
+    
 
     void Update()
     {
@@ -26,8 +39,7 @@ public class UIManager : MonoBehaviour
 
             if(GameManager.delayStartGame == 0)
             {
-                textDelay.text = "GO!";
-               
+                textDelay.text = "GO!"; 
             }
         }
         else
@@ -43,5 +55,27 @@ public class UIManager : MonoBehaviour
     void CoinUpdate()
     {
         fishCoinNumberText.text = GameManager.fishCoins.ToString();
+    }
+
+    void PauseGame()
+    {
+        pausePanel.SetActive(true);
+        Time.timeScale = 0.0f;
+        
+    }
+    private void ClosePauseScreen()
+    {
+        pausePanel.SetActive(false);
+        Time.timeScale = 1.0f;
+    }
+    private void RestartGame()
+    {
+        GameManager.fishCoins = 0;
+        SceneManager.LoadScene("GameScene");
+    }
+
+    private void BackToMenu()
+    {
+        SceneManager.LoadScene("MenuScene");
     }
 }
